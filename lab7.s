a53715dd3b071b28d24bbf92e131e2da783a8523
iguzman29 <iguzman@scu.edu>
2026-05-12 09:43:07 -0700

    Refactor PutNibble and GetNibble functions

--- lab7.s
+++ lab7.s
@@ -1,6 +1,11 @@
 /*
-    This code was written to support the book, "ARM Assembly for Embedded Applications",
-    by Daniel W. Lewis.
+ * File:			lab7c.s
+ *
+ * Author:			Isabella Guzman
+ *
+ * Last updated:	5/12/2026
+ *
+ * Description:		This file contains functions to implement equivalent replacements in assembly language for the  two functions, PutNibble and GetNibble, found in the C main program to store and retrieve individual nibbles within the array.
 */
         .syntax     unified
         .cpu        cortex-m4
@@ -16,9 +21,9 @@
         .thumb_func
         .align
 PutNibble:
-        PUSH    {R4, R5}            // Save registers to stay AAPCS compliant
+        PUSH    {R4, R5}            
         LSR     R3, R1, #1          // R3 = which / 2 (byte offset)
-        LDRB    R4, [R0, R3]        // R4 = current byte at the calculated address
+        LDRB    R4, [R0, R3]        
         AND     R1, R1, #1          // Check if index is odd (R1 = which & 1)
         CMP     R1, #1              
         BEQ     .PutOdd             // If odd, jump to handle high nibble
@@ -35,9 +40,9 @@ PutNibble:
         ORR     R4, R4, R2          // Or the value into the high nibble
 
 .PutDone:
-        STRB    R4, [R0, R3]        // Store the modified byte back to memory
-        POP     {R4, R5}            // Restore registers
-        BX      LR                  // Return
+        STRB    R4, [R0, R3]        
+        POP     {R4, R5}            
+        BX      LR                  
 
 // -----------------------------------------------------------------------------
 // uint32_t GetNibble(void *nibbles, uint32_t which)
@@ -50,11 +55,11 @@ PutNibble:
         .align
 GetNibble:
         LSR     R2, R1, #1          // R2 = which / 2 (byte offset)
-        LDRB    R0, [R0, R2]        // R0 = byte containing the desired nibble
+        LDRB    R0, [R0, R2]        
         ANDS    R1, R1, #1          // Check if index is odd (R1 = which & 1)
-        IT      NE                  // If Not Equal (odd index)...
-        LSRNE   R0, R0, #4          // ...shift right by 4 bits to move high nibble to low
+        IT      NE                  // If Not Equal (odd index)
+        LSRNE   R0, R0, #4          // Shift right by 4 bits to move high nibble to low
         AND     R0, R0, #0x0F       // Mask out everything except the low 4 bits
-        BX      LR                  // Return
+        BX      LR                  
 
         .end
