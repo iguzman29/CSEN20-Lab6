12936db218bc0481cecdb30914cf63a607ea9a41
iguzman29 <iguzman@scu.edu>
2026-05-19 09:18:58 -0700

    Add documentation for resistor.s
    This file contains assembly functions for equivalent replacements of C functions.

--- resistor.s
+++ resistor.s
@@ -1,1 +1,11 @@
+/*
+ * File:			resistor.s
+ *
+ * Author:			Isabella Guzman
+ *
+ * Last updated:	5/19/2026
+ *
+ * Description:		This file contains functions to implement equivalent replacements in assembly language for the following three functions found in the C main program: Mul32X10, which returns the 32-bit unsigned product of ten
+times its 32-bit argument, Mul64X10, which returns the 64-bit unsigned product of ten times its 64-bit argument, and Div32X10, which returns the 32-bit unsigned quotient of its argument divided by 10.
+*/
 
